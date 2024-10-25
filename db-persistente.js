fdc0d3285876871e18a2ac167492c650e424e57e
datadiego <juandiegomariscal@gmail.com>
2024-10-25 23:32:20 +0200

    creamos archivo .db

--- db-persistente.js
+++ db-persistente.js
@@ -5,7 +5,7 @@ const { readUser } = require('./crud');
 
 let db_aux = null;
 if(!fs.existsSync('database.db')){
-    db_aux = new Database(':memory:');
+    db_aux = new Database('database.db');
     db_aux.exec("CREATE TABLE users (user TEXT PRIMARY KEY, password TEXT)");
     db_aux.exec("INSERT INTO users (user, password) VALUES ('admin', '81dc9bdb52d04dc20036dbd8313ed055')");
     db_aux.exec("INSERT INTO users (user, password) VALUES ('pikachu', '04dac8afe0ca501587bad66f6b5ce5ad')");
